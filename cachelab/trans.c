7f782b0381422dd44372245c7c3e3830a40d5484
ax <ax@173>
2024-12-14 08:27:08 -0500

    32x32 optimition is ok

--- cachelab/trans.c
+++ cachelab/trans.c
@@ -19,9 +19,216 @@ int is_transpose(int M, int N, int A[N][M], int B[M][N]);
  *     searches for that string to identify the transpose function to
  *     be graded. 
  */
+// switch table store in the memory, do not use this
+void transpose_32x32_switch(int M, int N, int A[N][M], int B[M][N]) 
+{
+	int turn;
+	int row_start;
+	int column_start;
+	int temp0, temp1, temp2, temp3, temp4, temp5, temp6, temp7;
+	// split matrix to 8x8 matrix
+	for(turn=0; turn<16; turn++) {
+		row_start = (turn / 4) * 8;
+		column_start = (turn % 4) * 8;
+		for(M=row_start; M<row_start+8; M++) {
+			for(N=column_start; N<column_start+8; N++) {
+				switch(N % 8) {
+					case 0: temp0 = A[M][N]; break;
+					case 1: temp1 = A[M][N]; break;
+					case 2: temp2 = A[M][N]; break;
+					case 3: temp3 = A[M][N]; break;
+					case 4: temp4 = A[M][N]; break;
+					case 5: temp5 = A[M][N]; break;
+					case 6: temp6 = A[M][N]; break;
+					case 7: temp7 = A[M][N]; break;
+				}
+			}
+			for(N=column_start; N<column_start+8; N++) {
+				switch(N % 8) {
+					case 0: B[N][M] = temp0; break;
+					case 1: B[N][M] = temp1; break;
+					case 2: B[N][M] = temp2; break;
+					case 3: B[N][M] = temp3; break;
+					case 4: B[N][M] = temp4; break;
+					case 5: B[N][M] = temp5; break;
+					case 6: B[N][M] = temp6; break;
+					case 7: B[N][M] = temp7; break;
+				}
+			}
+		}
+	}
+}
+
+void transpose_32x32(int M, int N, int A[N][M], int B[M][N]) 
+{
+	int turn;
+	int row_start;
+	int column_start;
+	int temp0, temp1, temp2, temp3, temp4, temp5, temp6, temp7;
+	// split matrix to 8x8 matrix
+	for(turn=0; turn<16; turn++) {
+		row_start = (turn / 4) * 8;
+		column_start = (turn % 4) * 8;
+		for(M=row_start; M<row_start+8; M++) {
+			N = column_start;
+			temp0 = A[M][N]; N = N + 1;
+			temp1 = A[M][N]; N = N + 1;
+			temp2 = A[M][N]; N = N + 1;
+			temp3 = A[M][N]; N = N + 1;
+			temp4 = A[M][N]; N = N + 1;
+			temp5 = A[M][N]; N = N + 1;
+			temp6 = A[M][N]; N = N + 1;
+			temp7 = A[M][N]; N = N + 1;
+
+			N = column_start;
+			B[N][M] = temp0; N = N + 1;
+			B[N][M] = temp1; N = N + 1;
+			B[N][M] = temp2; N = N + 1;
+			B[N][M] = temp3; N = N + 1;
+			B[N][M] = temp4; N = N + 1;
+			B[N][M] = temp5; N = N + 1;
+			B[N][M] = temp6; N = N + 1;
+			B[N][M] = temp7; N = N + 1;
+		}
+	}
+}
+
+void transpose_64x64(int M, int N, int A[N][M], int B[M][N]) 
+{
+	int turn;
+	int row_start;
+	int column_start;
+	int temp0, temp1, temp2, temp3, temp4, temp5, temp6, temp7;
+	// split matrix to 8x8 matrix
+	for(turn=0; turn<128; turn++) {
+		row_start = (turn / 16) * 8;
+		column_start = (turn % 16) * 4;
+		for(M=row_start; M<row_start+4; M++) {
+			N = column_start;
+			temp0 = A[M][N]; N = N + 1;
+			temp1 = A[M][N]; N = N + 1;
+			temp2 = A[M][N]; N = N + 1;
+			temp3 = A[M][N]; N = N + 1;
+
+			M += 4;
+			N = column_start;
+			temp4 = A[M][N]; N = N + 1;
+			temp5 = A[M][N]; N = N + 1;
+			temp6 = A[M][N]; N = N + 1;
+			temp7 = A[M][N]; N = N + 1;
+
+			M -= 4;
+			N = column_start;
+			B[N][M] = temp0; N = N + 1;
+			B[N][M] = temp1; N = N + 1;
+			B[N][M] = temp2; N = N + 1;
+			B[N][M] = temp3; N = N + 1;
+
+			M += 4;
+			N = column_start;
+			B[N][M] = temp4; N = N + 1;
+			B[N][M] = temp5; N = N + 1;
+			B[N][M] = temp6; N = N + 1;
+			B[N][M] = temp7; N = N + 1;
+			
+			M -= 4;
+		}
+	}
+}
+
+void transpose_64x64_v2(int M, int N, int A[N][M], int B[M][N]) 
+{
+	int turn;
+	int row_start;
+	int column_start;
+	int temp0, temp1, temp2, temp3, temp4, temp5, temp6, temp7;
+	// split matrix to 8x8 matrix
+	for(turn=0; turn<64; turn++) {
+		row_start = (turn / 8) * 8;
+		column_start = (turn % 8) * 8;
+		for(M=row_start; M<row_start+8; M++) {
+			N = column_start;
+			temp0 = A[M][N]; N = N + 1;
+			temp1 = A[M][N]; N = N + 1;
+			temp2 = A[M][N]; N = N + 1;
+			temp3 = A[M][N]; N = N + 1;
+			temp4 = A[M][N]; N = N + 1;
+			temp5 = A[M][N]; N = N + 1;
+			temp6 = A[M][N]; N = N + 1;
+			temp7 = A[M][N]; N = N + 1;
+			
+			if(M < row_start+4) {
+				N = column_start;
+				B[N][M] = temp0; N = N + 1;
+				B[N][M] = temp1; N = N + 1;
+				B[N][M] = temp2; N = N + 1;
+				B[N][M] = temp3; N = N + 1;
+	
+				M = M + 4;
+				N = column_start;
+				B[N][M] = temp4; N = N + 1;
+				B[N][M] = temp5; N = N + 1;
+				B[N][M] = temp6; N = N + 1;
+				B[N][M] = temp7; N = N + 1;
+			}
+			else {
+				N = column_start + 4;
+				B[N][M] = temp0; N = N + 1;
+				B[N][M] = temp1; N = N + 1;
+				B[N][M] = temp2; N = N + 1;
+				B[N][M] = temp3; N = N + 1;
+	
+				M = M + 4;
+				N = column_start+4;
+				B[N][M] = temp4; N = N + 1;
+				B[N][M] = temp5; N = N + 1;
+				B[N][M] = temp6; N = N + 1;
+				B[N][M] = temp7; N = N + 1;
+
+			}
+			M = M - 4;
+		}
+		// tackle with 8x8, mainly for 4x4 diag
+		// change you shang 4x4 he zuo xia 4x4
+		for(N=column_start; N<column_start+4; N++) {
+			M = row_start;
+			temp0 = B[N][M+4];
+			temp1 = B[N][M+5];
+			temp2 = B[N][M+6];
+			temp3 = B[N][M+7];
+		
+			N = N + 4;
+			temp4 = B[N][M]; M = M + 1;
+			temp5 = B[N][M]; M = M + 1;
+			temp6 = B[N][M]; M = M + 1;
+			temp7 = B[N][M]; 
+
+			B[N][M] = temp7; M = M - 1;
+			B[N][M] = temp6; M = M - 1;
+			B[N][M] = temp5; M = M - 1;
+			B[N][M] = temp4; 
+
+			B[N][M+4] = temp0;
+			B[N][M+5] = temp1;
+			B[N][M+6] = temp2;
+			B[N][M+7] = temp3;
+			
+			N = N - 4;
+
+		}
+	}
+}
+
+
 char transpose_submit_desc[] = "Transpose submission";
 void transpose_submit(int M, int N, int A[N][M], int B[M][N])
 {
+	if((M==32) && (N==32)) {
+		transpose_32x32(M, N, A, B);
+	}
+	else if((M==64) && (N==64)) {
+		transpose_64x64_v2(M, N, A, B);
+	}
 }
 
 /* 
